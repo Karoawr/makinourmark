aa0d604f38b3b2ae4c0ee234ced040a522bf88d2
markayiah <mark.ayiah@student.allenisd.org>
2021-04-29 12:15:09 -0500

    Saved state with blue background

--- ScenesShellBasic/Sources/ScenesShell/Background.swift
+++ ScenesShellBasic/Sources/ScenesShell/Background.swift
@@ -7,9 +7,20 @@ import Igis
 
 
 class Background : RenderableEntity {
+    let fillStyle = FillStyle(color:Color(.blue))
+    let rectangle : Rectangle
+    
+    init() {
+        // Using a meaningful name can be helpful for debugging
+        rectangle = Rectangle(rect:Rect(), fillMode: .fill)
+        super.init(name:"Background")
+    }
+    
+    override func setup(canvasSize:Size, canvas:Canvas) {
+        rectangle.rect.size = canvasSize
+    }
 
-      init() {
-          // Using a meaningful name can be helpful for debugging
-          super.init(name:"Background")
-      }
+    override func render(canvas:Canvas) {
+        canvas.render(fillStyle, rectangle)
+    }
 }
